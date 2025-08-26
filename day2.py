fcebc64704267396c8056dde2564f9b631ab593c
Prince K.C <princekc645@gmail.com>
2025-08-26 09:50:03 +0545

    Notes of Day2

---
+++ day2.py
@@ -0,0 +1,28 @@
+"""
+Comparision operator
+
+print(5>=4)
+print(5>=5)
+print(5>=8)
+print()
+print(5<=4)
+print(5<=5)
+print(5<=8)
+
+Logical operator
+
+print(True or False)
+print(False or True)
+print(True or True)
+print(False or False)
+print()
+print(True and False)
+print(False and True)
+print(True and True)
+print(False and False)
+
+Operator Precedence
+
+print(True and True and not True or False or not False and True)
+
+"""
