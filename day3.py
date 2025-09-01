4ede0c997e872f3b25af80112f69dd0453250e1a
Prince K.C <princekc645@gmail.com>
2025-09-01 09:53:06 +0545

    day3 added

---
+++ day3.py
@@ -0,0 +1,38 @@
+"""
+concatinate
+name = "Ram"
+greeting = 'Hello'
+message = "Python is fun!"
+print(greeting+name)
+
+single = 'Hello'
+double = "World"
+multiline = "This is
+a multiline
+string"
+print(multiline)
+
+text = "Python"
+print(len(text))  # 6
+
+
+
+# indexing and slicing
+
+    #index
+        #a[start:stop:step(optional)]
+        # default is [0:len:1]
+a="This is python"
+print(a[:5])
+
+print(a[::-1]) #reverse
+
+#Strings are immutable
+
+a="hello"
+b=a.upper()
+print(a.upper())
+print(a)#imutable
+print(b)
+
+"""
